7af57d79fbbedd19ad0c5c6e86bee7579beaeb5a
martinagiammillari <martigiammi@gmail.com>
2026-01-20 16:59:20 +0100

    feat: milestone 3 done

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from "express";
 import moviesRouter from "./routers/movies.js"
 import errorHandler from "./middlewares/errorHandler.js";
 import notFound from "./middlewares/notFound.js";
-
+import cors from "cors";
 
 const app = express();
 const port = process.env.SERVER_PORT;
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 
 
 app.use("/api/movies", moviesRouter);
