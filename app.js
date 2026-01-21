3a336270666d9948af455a10ba0e721d861e354b
martinagiammillari <martigiammi@gmail.com>
2026-01-21 14:25:25 +0100

    feat: images done

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from "express";
-import moviesRouter from "./routers/movies.js"
+import cors from "cors";
+import moviesRouter from "./routers/movies.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import notFound from "./middlewares/notFound.js";
-import cors from "cors";
 
 const app = express();
-const port = process.env.SERVER_PORT;
-app.use(cors({
-    origin:"http://localhost:5173"
-}))
+const port = process.env.SERVER_PORT || 3600;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
+
+
+app.use(express.static("public"));
 
 app.use("/api/movies", moviesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port,() => {
-    console.log(`server is listening on ${port}`);
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
 });
