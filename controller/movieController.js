7def945cd65c847ad3696fba43e59dffb987d9ec
martinagiammillari <martigiammi@gmail.com>
2026-01-22 09:34:59 +0100

    feat: reviews done

--- controller/movieController.js
+++ controller/movieController.js
@@ -18,21 +18,57 @@ function index(req, res, next) {
 
 // SHOW
 function show(req, res, next) {
-    const id = req.params.id;
-    const query = "SELECT * FROM movies WHERE id = ?";
+  const id = req.params.id;
 
-    connection.query(query, [id], (err, results) => {
-        if (err) return next(err);
+  const query = `
+    SELECT
+      movies.*,
+      reviews.id AS review_id,
+      reviews.name,
+      reviews.vote,
+      reviews.\`text\`
+    FROM movies
+    LEFT JOIN reviews
+      ON reviews.movie_id = movies.id
+    WHERE movies.id = ?
+  `;
 
-        if (results.length === 0) {
-            return res.status(404).json({
-                message: "Movie not found"
-            });
-        }
+  connection.query(query, [id], (err, results) => {
+    if (err) return next(err);
 
-        res.json(results[0]);
+    if (results.length === 0) {
+      return res.status(404).json({
+        message: "Movie not found"
+      });
+    }
+
+    // Creo l'oggetto movie base
+    const movie = {
+      id: results[0].id,
+      title: results[0].title,
+      director: results[0].director,
+      abstract: results[0].abstract,
+      genre: results[0].genre,
+      release_year: results[0].release_year,
+      image: results[0].image,
+      reviews: []
+    };
+
+    // Aggiungo le recensioni
+    results.forEach((row) => {
+      if (row.review_id) {
+        movie.reviews.push({
+          id: row.review_id,
+          name: row.name,
+          vote: row.vote,
+          text: row.text 
+        });
+      }
     });
+
+    res.json(movie);
+  });
 }
 
 
-export default {index, show};
+export default { index, show };
